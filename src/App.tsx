24ad5456b4d383a6434c2c81014a8c40b1e3597e
jpita <joseacureropita@gmail.com>
2024-08-20 23:35:27 -0400

    config and publish

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import "./App.css";
+import { Button } from "./components/button";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Button></Button>
+    </>
+  );
+}
+
+export default App;
